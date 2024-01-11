a74bbe891000da2a4a578c877557eb2e861b8930
wlagos3 <wlagos3@outlook.com>
2024-01-10 22:49:50 -0500

    private routes

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -1,8 +1,7 @@
 import React, {useEffect} from 'react';
 import {Navigate} from 'react-router-dom';
-import {useSelector} from "react-redux";
 import {fetchUser, selectUserState} from "./redux/userSlice";
-import {useAppDispatch} from "./redux/store";
+import {useAppDispatch, useAppSelector} from "./redux/store";
 
 
 interface PrivateRouteProps {
@@ -11,12 +10,12 @@ interface PrivateRouteProps {
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({element}) => {
     const dispatch = useAppDispatch();
-    const sessionState = useSelector(selectUserState)
+    const sessionState = useAppSelector(selectUserState)
     console.log(sessionState)
     const isAuthenticated = sessionState && sessionState.status !== 'failed'
     useEffect(() => {
         dispatch(fetchUser())
-    }, [isAuthenticated]);
+    }, [dispatch, isAuthenticated]);
 
     return isAuthenticated ? element : <Navigate to="/login"/>;
 };
