551a8f12c4a039f4c9ce773a3bd947ff49b7ce4b
wlagos3 <wlagos3@outlook.com>
2024-01-13 02:20:56 -0500

    change backend url

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {Button, TextField, Container, Typography, Paper} from '@mui/material';
 import {useNavigate} from 'react-router-dom';
 import {loginUser} from "./redux/userSlice";
