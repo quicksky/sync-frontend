fac6098841b9cc2674d27f15724eb45d8a01f853
wlagos3 <wlagos3@outlook.com>
2024-01-16 03:22:47 -0500

    Update LoginPage.tsx

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -37,7 +37,7 @@ const LoginPage: React.FC = () => {
                     mt: 10,
                     display: 'flex',
                     flexDirection: 'column',
-                    justifyContent: 'center'
+                    justifyContent: 'center',
                     alignItems: 'center',
                 }}
             >
